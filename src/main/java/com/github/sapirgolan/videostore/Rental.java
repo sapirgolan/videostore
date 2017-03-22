4f6073297d015ef81db2e04e79b9fbd1927aed93
Sapir Golan <s.golan@sap.com>
2017-03-22 13:14:09 +0200

    Moved getCharge() to Movie.java

--- src/main/java/com/github/sapirgolan/videostore/Rental.java
+++ src/main/java/com/github/sapirgolan/videostore/Rental.java
@@ -2,7 +2,7 @@ package com.github.sapirgolan.videostore;
 
 public class Rental {
 
-    private Movie movie;
+    Movie movie;
     private int daysRented;
 
     public Rental(final Movie movie, final int daysRented) {
@@ -18,28 +18,6 @@ public class Rental {
         return movie;
     }
 
-    public double getCharge() {
-        double thisAmount = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (getDaysRented() > 2) {
-                    thisAmount += (getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (getDaysRented() > 3) {
-                    thisAmount += (getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-        return thisAmount;
-    }
-
     public int getFrequentRenterPoints() {
         int frequentRenterPoints = 1;
 
@@ -47,5 +25,9 @@ public class Rental {
             frequentRenterPoints++;
         }
         return frequentRenterPoints;
+    }
+
+    public double getCharge() {
+        return movie.getCharge(daysRented);
     }
 }
