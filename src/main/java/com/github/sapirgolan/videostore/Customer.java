85b9c0a4289a4ce907a2cd615842c6d3281c5499
Sapir Golan <s.golan@sap.com>
2017-03-22 08:18:01 +0200

    move getAmount to Rental.java

--- src/main/java/com/github/sapirgolan/videostore/Customer.java
+++ src/main/java/com/github/sapirgolan/videostore/Customer.java
@@ -29,7 +29,7 @@ public class Customer {
             final Rental each = (Rental) rentals.nextElement();
 
             // determines the amount for each line
-            double thisAmount = getThisAmount(each);
+            double thisAmount = each.getCharge();
 
             frequentRenterPoints++;
 
@@ -48,25 +48,4 @@ public class Customer {
         return result;
     }
 
-    private double getThisAmount(Rental rental) {
-        double thisAmount = 0;
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (rental.getDaysRented() > 2) {
-                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (rental.getDaysRented() > 3) {
-                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-        return thisAmount;
-    }
 }
